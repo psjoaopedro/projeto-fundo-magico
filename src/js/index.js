d2496ef93fe9af6817fd93240a95e92f8a6fe83e
João Pedro <psjoaopedro@gmail.com>
2025-10-04 23:41:30 -0300

    finalizando funções do html e finalizando conexão com n8n

--- src/js/index.js
+++ src/js/index.js
@@ -13,31 +13,59 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const form = document.querySelector('.form-group')
     const textArea = document.getElementById('description')
+    const htmlCode = document.getElementById('html-code')
+    const cssCode = document.getElementById('css-code')
+    const preview = document.getElementById('preview-section')
 
     form.addEventListener('submit', async function (event) {
         event.preventDefault()
 
         const description = textArea.value.trim()
 
-        if(!description){
+        if (!description) {
             return
         }
 
         setLoading(true)
 
-        try{
+        try {
 
-            const response = await fetch('link do n8n', {
+            const response = await fetch('https://melancia123.app.n8n.cloud/webhook/gerador-fundo', {
                 method: 'POST',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify({description})
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ description })
             })
 
+            const data = await response.json()
+            htmlCode.textContent = data.code || ''
+            cssCode.textContent = data.style || ''
 
-        }catch{
+            preview.style.display = 'block'
+            preview.innerHTML = data.code || ''
 
-        }finally{
+            let styleTag = document.getElementById('dynamic-style')
 
+            if (styleTag) styleTag.remove()
+
+            if (data.style) {
+                styleTag = document.createElement('style')
+
+                styleTag.id = 'dynamic-style'
+
+                styleTag.textContent = data.style
+
+                document.head.appendChild(styleTag)
+            }
+
+        } catch (error) {
+            console.error('Erro ao gerar o fundo: ', error)
+            htmlCode.textContent = "Não consegui gerar o código HTML, tente novamente"
+            cssCode.textContent = 'Não consegui gerar o código CSS, tente novamente'
+            preview.innerHTML = ''
+
+
+        } finally {
+            setLoading(false)
         }
 
     })
