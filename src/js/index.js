2034c93f7d0c14a4bbb1126e3b2ad0b9b305800a
João Pedro <psjoaopedro@gmail.com>
2025-10-04 21:28:56 -0300

    inicializando funções no js, começando pelo recarregamento automático da página

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function (){
+
+    const form = document.querySelector('.form-group')
+
+    form.addEventListener('submit', function(event){
+        event.preventDefault()
+    })
+})
