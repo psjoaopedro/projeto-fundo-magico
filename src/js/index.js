477d06dc034d691cfb1c1ec58d359cf051d52c3b
João Pedro <psjoaopedro@gmail.com>
2025-10-04 21:53:35 -0300

    preparando codigo para automações no N8N

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,44 @@
-document.addEventListener('DOMContentLoaded', function (){
+function setLoading(isLoading) {
+    const btnSpan = document.getElementById('generate-btn')
+
+    if (isLoading) {
+        btnSpan.innerHTML = 'Gerando Background ...'
+    } else {
+        btnSpan.innerHTML = 'Gerar Background Mágico'
+    }
+
+}
+
+document.addEventListener('DOMContentLoaded', function () {
 
     const form = document.querySelector('.form-group')
+    const textArea = document.getElementById('description')
 
-    form.addEventListener('submit', function(event){
+    form.addEventListener('submit', async function (event) {
         event.preventDefault()
+
+        const description = textArea.value.trim()
+
+        if(!description){
+            return
+        }
+
+        setLoading(true)
+
+        try{
+
+            const response = await fetch('link do n8n', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({description})
+            })
+
+
+        }catch{
+
+        }finally{
+
+        }
+
     })
 })
